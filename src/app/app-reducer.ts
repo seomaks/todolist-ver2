0b15d52fc7ca16fcd48b8dd5e750aa37491c001d
seomaks <m.voevodin88@gmail.com>
2022-01-22 11:16:46 +0300

    Loading action

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,23 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+  status: 'loading' as RequestStatusType
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+  switch (action.type) {
+    case 'APP/SET-STATUS':
+      return {...state, status: action.status}
+    default:
+      return state
+  }
+}
+
+// actions
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+
+// types
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+type ActionsType = SetAppStatusActionType
